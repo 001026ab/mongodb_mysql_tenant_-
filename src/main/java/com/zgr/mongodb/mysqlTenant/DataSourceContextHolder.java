484a4eafa293b04523b4c18c55c6194498554148
001026ab <1176754512@qq.com>
2022-04-19 18:30:49 +0800

    DataSourceContextHolder与DynamicDataSource其实可以合并

--- src/main/java/com/zgr/mongodb/mysqlTenant/DataSourceContextHolder.java
+++ src/main/java/com/zgr/mongodb/mysqlTenant/DataSourceContextHolder.java
@@ -2,16 +2,19 @@ package com.zgr.mongodb.mysqlTenant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
  * @author zgr
  * @version 1.0
  * @date 2022/4/19 17:06
  * 操作工具类，可以使用setDataSource方法切换数据源
+ *
+ * extends AbstractRoutingDataSource
  */
 
 
-public class DataSourceContextHolder {
+public class DataSourceContextHolder  {
     private static Logger logger = LoggerFactory.getLogger(DataSourceContextHolder.class);
 
     // 使用ThreadLocal线程安全的使用变量副本
@@ -38,4 +41,9 @@ public class DataSourceContextHolder {
     public static void clearDataSource() {
         CONTEXT_HOLDER.remove();
     }
+
+    /*@Override
+    protected Object determineCurrentLookupKey() {
+        return getDataSource();
+    }*/
 }
